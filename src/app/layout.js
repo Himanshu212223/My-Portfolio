51ef22ef557382359495d528716f088cde5655f5
Himanshu <himanshu212223@gmail.com>
2025-07-25 00:12:27 +0530

    updated layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,11 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <Script src="https://kit.fontawesome.com/9fa326583e.js" crossOrigin="anonymous"></Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       </Head>
+        <Script src="https://kit.fontawesome.com/9fa326583e.js" crossOrigin="anonymous"></Script>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         
         <div className="main-page">
